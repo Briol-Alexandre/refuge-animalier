b6e1e5ea56ad98a9de9522d8fb611968111d3723
Briol-Alexandre <145542906+Briol-Alexandre@users.noreply.github.com>
2025-11-19 09:28:21 +0100

    Added responsiveness to the header section

--- resources/views/components/header-image.blade.php
+++ resources/views/components/header-image.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-72 h-72 rounded-btn mb-normal relative">
+<div class="min-h-72 h-72 lg:min-h-90 xl:min-h-[500px] rounded-btn mb-normal relative">
     <img src="{{ asset('assets/img/home-img.png') }}"
          alt="Image d'un chien et d'un chat"
          class="w-full h-full object-cover rounded-btn">
@@ -9,15 +9,15 @@
     </x-bubble>
     <x-bubble :color="'text-main-yellow'" :opacity="'bg-main-yellow-opacity'" :label="'Animaux adoptés'"
               :num="'53'" :class="'bubble2 group-bubble1'" :padding="false">
-        <x-svgs.paw :class="'w-5 h-5'"/>
+        <x-svgs.paw :class="'w-5 h-5 xl:w-8 xl:h-8'"/>
     </x-bubble>
     <x-bubble :color="'text-main-yellow'" :opacity="'bg-main-yellow-opacity'" :label="'Animaux adoptés'"
               :num="'53'" :class="'bubble3 group-bubble2'" :padding="false">
-        <x-svgs.paw :class="'w-5 h-5'"/>
+        <x-svgs.paw :class="'w-5 h-5 xl:w-8 xl:h-8'"/>
     </x-bubble>
     <x-bubble :color="'text-main-yellow'" :opacity="'bg-main-yellow-opacity'" :label="'Animaux adoptés'"
               :num="'53'" :class="'bubble4 group-bubble2'" :padding="false">
-        <x-svgs.paw :class="'w-5 h-5'"/>
+        <x-svgs.paw :class="'w-5 h-5 xl:w-8 xl:h-8'"/>
     </x-bubble>
 
 </div>
--- resources/views/components/home-page/header-main-page.blade.php
+++ resources/views/components/home-page/header-main-page.blade.php
@@ -1,17 +1,23 @@
-<section aria-labelledby="home-page-title" class="grid-basic relative">
-    <x-svgs.bg-shape :blue="false" :class="'-top-92 -right-60 -z-10'"/>
+<section aria-labelledby="home-page-title" class="relative">
+    <x-svgs.bg-shape :blue="false" :class="'-top-92 -right-60 -z-10 '"/>
     <x-svgs.bg-shape :blue="true" :class="'-z-20 top-16'"/>
-    <div class="col-start-2 col-end-7">
-        <x-header-image/>
-        <h2 id="home-page-title" class="big-title">
-            Un <strong>foyer</strong> pour chaque patte, un <strong>sourire</strong> pour chaque
-            <strong>cœur</strong>
-        </h2>
-        <p class="text-content text-center my-5">Chaque jour, notre équipe de bénévoles s’engage à offrir à chaque
-            compagnon une seconde chance, beaucoup d’amour, et un foyer où il pourra s’épanouir.</p>
-        <div class="flex flex-col gap-4">
-            <x-button :route="''" :label="'Adoptez !'" :type="'light'"/>
-            <x-button :route="''" :label="'En savoir plus'" :type="'dark'"/>
+    <div class="grid-basic items-center">
+        <div class="col-start-2 col-end-7 md:col-start-7 md:col-end-12">
+            <x-header-image/>
+        </div>
+        <div class="col-start-2 col-end-7 md:row-start-1 lg:col-end-6">
+            <h2 id="home-page-title" class="big-title">
+                Un <strong>foyer</strong> pour chaque patte, un <strong>sourire</strong> pour chaque
+                <strong>cœur</strong>
+            </h2>
+            <p class="text-content text-center md:text-left my-5">Chaque jour, notre équipe de bénévoles
+                s’engage à offrir à
+                chaque
+                compagnon une seconde chance, beaucoup d’amour, et un foyer où il pourra s’épanouir.</p>
+            <div class=" flex flex-col md:flex-row gap-4">
+                <x-button :route="''" :label="'Adoptez !'" :type="'light'"/>
+                <x-button :route="''" :label="'En savoir plus'" :type="'dark'"/>
+            </div>
         </div>
     </div>
 </section>
