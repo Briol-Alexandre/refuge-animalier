2eee3f5f339383530d938318ea968cba1cf5ead3
Briol-Alexandre <145542906+Briol-Alexandre@users.noreply.github.com>
2025-12-27 13:09:09 +0100

    Fix tabulation (might not be the best way to do it but it works)

--- resources/js/components/widget/Modal.vue
+++ resources/js/components/widget/Modal.vue
@@ -7,14 +7,22 @@
         leave-from-class="opacity-100"
         leave-to-class="opacity-0"
     >
-        <div @keydown.esc="closeModal" @click.self="closeModal"
-             class="absolute w-screen h-screen bg-softGray/30 backdrop-blur-xs top-0 left-0 " :class="index"
-             v-if="condition">
-            <div class="absolute top-1/2 left-1/2 -translate-1/2 bg-white rounded-2xl shadow-sm p-10 w-3/4"
-                 :class="modalClasses">
-
-                <button @click="closeModal" class="absolute right-10 top-10 hover:cursor-pointer"
-                        :class="closeBtnClasses">
+        <div
+            v-if="condition"
+            @keydown.esc="closeModal"
+            @click.self="closeModal"
+            class="absolute w-screen h-screen bg-softGray/30 backdrop-blur-xs top-0 left-0"
+            :class="index"
+        >
+            <div
+                class="absolute top-1/2 left-1/2 -translate-1/2 bg-white rounded-2xl shadow-sm p-10 w-3/4"
+                :class="modalClasses"
+            >
+                <button
+                    @click="closeModal"
+                    class="absolute right-10 top-10 hover:cursor-pointer"
+                    :class="closeBtnClasses"
+                >
                     <Close />
                 </button>
                 <slot />
@@ -27,22 +35,30 @@
 import Close from '@/components/svgs/Close.vue';
 
 export default {
-    name: '',
+    name: 'Modal',
     components: { Close },
     props: ['condition', 'index', 'modalClasses', 'closeBtnClasses'],
-
-    data() {
-        return {};
+    watch: {
+        condition(isOpen) {
+            if (isOpen) {
+                this.$nextTick(() => {
+                    const appElement = document.getElementById('app'); // ou votre élément racine
+                    if (appElement) {
+                        appElement.setAttribute('inert', '');
+                    }
+                });
+            } else {
+                const appElement = document.getElementById('app');
+                if (appElement) {
+                    appElement.removeAttribute('inert');
+                }
+            }
+        }
     },
-
     methods: {
         closeModal() {
             this.$emit('close');
         }
-    }
+    },
 };
 </script>
-
-<style>
-
-</style>
