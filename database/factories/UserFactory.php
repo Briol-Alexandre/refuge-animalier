299dee43fb6cc0162fe158e27f296c76233a7c18
Briol-Alexandre <145542906+Briol-Alexandre@users.noreply.github.com>
2025-12-31 17:52:06 +0100

    Build

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,18 +24,19 @@ class UserFactory extends Factory
     {
         $image = 'assets/img/default.jpg';
         $imageJson =[$image => []];
+        $faker = \Faker\Factory::create('fr_BE');
         return [
             'avatar' => json_encode($imageJson),
-            'tel' => fake()->phoneNumber(),
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'tel' => $faker->phoneNumber(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= 'password',
             'remember_token' => Str::random(10),
             'two_factor_secret' => Str::random(10),
             'two_factor_recovery_codes' => Str::random(10),
             'two_factor_confirmed_at' => now(),
-            'created_at' => fake()->dateTimeBetween('-5 years', 'now'),
+            'created_at' => $faker->dateTimeBetween('-5 years', 'now'),
         ];
     }
 
